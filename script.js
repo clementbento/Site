30a29831f038f417db54da764450bdc6b3f4f7f2
clementbento <146069625+clementbento@users.noreply.github.com>
2023-11-09 18:16:50 +0100

    maj

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 document.getElementById("helloText").addEventListener("click", function() {
     alert("Hello World Clicked!");
 });
+
+document.getElementById("button1").addEventListener("click", function() {
+    alert("Bouton 1 cliqué!");
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    alert("Bouton 2 cliqué!");
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+    alert("Bouton 3 cliqué!");
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+    alert("Bouton 4 cliqué!");
+});
