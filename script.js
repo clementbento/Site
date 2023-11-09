425a1d7d06ff9837c2a4950c99f6db1526eca8bc
clementbento <146069625+clementbento@users.noreply.github.com>
2023-11-09 22:21:04 +0100

    maj

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// Ajoute une interaction simple
-document.getElementById("helloText").addEventListener("click", function() {
-    alert("Hello World Clicked!");
-});
-
-document.getElementById("button1").addEventListener("click", function() {
-    alert("Bouton 1 cliqué!");
-});
-
-document.getElementById("button2").addEventListener("click", function() {
-    alert("Bouton 2 cliqué!");
-});
-
-document.getElementById("button3").addEventListener("click", function() {
-    alert("Bouton 3 cliqué!");
-});
-
-document.getElementById("button4").addEventListener("click", function() {
-    alert("Bouton 4 cliqué!");
+document.addEventListener("DOMContentLoaded", function() {
+    const grid = document.getElementById("grid");
+    
+    for (let i = 0; i < 100; i++) {
+        const square = document.createElement("div");
+        square.className = "grid-item";
+        square.addEventListener("click", toggleColor);
+        grid.appendChild(square);
+    }
+    
+    function toggleColor() {
+        this.style.backgroundColor = this.style.backgroundColor === "black" ? "white" : "black";
+    }
 });
