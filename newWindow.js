1fd34f2c2dfb9d5b2faa8f0287beac0a8ac5e521
clementbento <146069625+clementbento@users.noreply.github.com>
2024-01-04 01:54:44 +0100

    MAJ

--- newWindow.js
+++ newWindow.js
@@ -1,15 +1,88 @@
-function updateDatabase() {
-  const dataInput = document.getElementById('dataInput');
-  const newValue = dataInput.value.trim(); // trim pour supprimer les espaces avant et après le texte
+document.addEventListener("DOMContentLoaded", function () {
+  const db = initializeFirebase();
 
-  // Assurez-vous que la valeur n'est pas vide
-  if (newValue !== "") {
-    // Mettez à jour la base de données avec la nouvelle valeur
-    const urlParams = new URLSearchParams(window.location.search);
-    const day = urlParams.get('day');
-    const month = urlParams.get('month');
-    
-    // Replace these values with your Firebase project configuration
+  // Récupérer les paramètres d'URL pour le jour et le mois
+  const urlParams = new URLSearchParams(window.location.search);
+  const day = urlParams.get('day');
+  const month = urlParams.get('month');
+
+  // Mettre à jour le titre de la fenêtre avec le jour et le mois
+  document.getElementById('newWindowTitle').textContent = `Données du ${day} ${month}`;
+
+  // Mettre à jour les attributs data-day et data-month
+  document.getElementById('dayData').setAttribute('data-day', day);
+  document.getElementById('monthData').setAttribute('data-month', month);
+
+  // Mettre à jour la liste des valeurs associées au jour et au mois
+  updateDatabaseValuesList(day, month);
+  updateNotepad(day, month);
+
+  const notePadInput = document.getElementById('notePadInput');
+  notePadInput.addEventListener('input', function () {
+    const notePadValue = notePadInput.value.trim();
+    saveNotePadValue(day, month, notePadValue);
+  });
+
+  function saveNotePadValue(day, month, notePadValue) {
+    const db = firebase.firestore();
+    const docRef = db.collection('calendarData').doc(`${month}-${day}`);
+  
+    // Récupérer les données existantes
+    docRef.get()
+      .then((docSnapshot) => {
+        if (docSnapshot.exists) {
+          const existingData = docSnapshot.data().values || [];
+          
+          // Mettre à jour le document avec les valeurs existantes et la nouvelle valeur du bloc-note
+          docRef.set({
+            values: existingData,
+            notePadValue: notePadValue
+          })
+            .then(function () {
+              console.log("NotePad value successfully written!");
+            })
+            .catch(function (error) {
+              console.error("Error writing NotePad value: ", error);
+            });
+        } else {
+          // Si le document n'existe pas, créer un nouveau document avec la valeur du bloc-note
+          docRef.set({
+            notePadValue: notePadValue
+          })
+            .then(function () {
+              console.log("NotePad value successfully written!");
+            })
+            .catch(function (error) {
+              console.error("Error writing NotePad value: ", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Error getting document: ", error);
+      });
+  }
+
+  function updateNotepad(day, month) {
+    const db = firebase.firestore();
+    const docRef = db.collection('calendarData').doc(`${month}-${day}`);
+  
+    docRef.get()
+      .then((doc) => {
+        if (doc.exists) {
+          const notePadValue = doc.data().notePadValue || "";
+          const notePadInput = document.getElementById('notePadInput');
+          notePadInput.value = notePadValue;
+        }
+      })
+      .catch((error) => {
+        console.error("Error getting document: ", error);
+      });
+  }
+  
+});
+
+function initializeFirebase() {
+  if (!firebase.apps.length) {
     const firebaseConfig = {
       apiKey: "AIzaSyC8C45EXE-CmqjBxC6cYOo5c4EdKnXnl7E",
       authDomain: "calendrier-d3dc8.firebaseapp.com",
@@ -20,28 +93,117 @@ function updateDatabase() {
       measurementId: "G-L7VTE6ZM5W"
     };
 
-    // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+  }
 
-    // Get a reference to the Firestore database
+  return firebase.firestore();
+}
+
+function updateDatabase() {
+  const dataInput = document.getElementById('dataInput');
+  const newValue = dataInput.value.trim();
+
+  if (newValue !== "") {
+    const day = document.getElementById('dayData').getAttribute('data-day');
+    const month = document.getElementById('monthData').getAttribute('data-month');
+
     const db = firebase.firestore();
-
-    // Get the reference to the specific document in Firestore
     const docRef = db.collection('calendarData').doc(`${month}-${day}`);
 
-    // Update the document with the new value
+    // Ajouter une nouvelle valeur au document avec le format Jour-Mois
     docRef.set({
-      value: newValue
-    })
-    .then(function () {
+      values: firebase.firestore.FieldValue.arrayUnion({ value: newValue, checked: false })
+    }, { merge: true })
+    .then(() => {
       console.log("Document successfully written!");
-      // Vous pouvez également mettre à jour l'affichage sur la page si nécessaire
-      document.getElementById('valueData').textContent = `Value: ${newValue}`;
+
+      // Mettre à jour la liste des valeurs
+      updateDatabaseValuesList(day, month);
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.error("Error writing document: ", error);
     });
   } else {
     alert("Veuillez entrer une valeur valide.");
   }
+}
+
+function updateDatabaseValuesList(day, month) {
+  const db = firebase.firestore();
+  const valuesList = document.getElementById('databaseValuesList');
+
+  // Efface la liste actuelle
+  valuesList.innerHTML = "";
+
+  // Obtient le document correspondant au jour et au mois
+  const docRef = db.collection('calendarData').doc(`${month}-${day}`);
+
+  docRef.get()
+    .then((doc) => {
+      if (doc.exists) {
+        const values = doc.data().values || [];
+
+        // Ajoute les valeurs à la liste des valeurs globales
+        values.forEach((item) => {
+          const listItem = document.createElement('li');
+
+          // Ajout d'une case à cocher à côté de chaque valeur
+          const checkbox = document.createElement('input');
+          checkbox.type = 'checkbox';
+          checkbox.checked = item.checked;
+          checkbox.addEventListener('change', function () {
+            // Mettre à jour l'état "checked" dans la base de données
+            updateCheckedState(day, month, item.value, checkbox.checked);
+          });
+
+          listItem.appendChild(checkbox);
+          
+          // Ajout de la valeur à côté de la case à cocher
+          const valueSpan = document.createElement('span');
+          valueSpan.textContent = item.value;
+          listItem.appendChild(valueSpan);
+
+          valuesList.appendChild(listItem);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting document: ", error);
+    });
+}
+
+function updateCheckedState(day, month, value, checked) {
+  const db = firebase.firestore();
+  const docRef = db.collection('calendarData').doc(`${month}-${day}`);
+
+  docRef.get()
+    .then((doc) => {
+      if (doc.exists) {
+        const values = doc.data().values || [];
+
+        // Mettre à jour l'état "checked" pour la valeur spécifique
+        const updatedValues = values.map((item) => {
+          if (item.value === value) {
+            return { value: item.value, checked: checked };
+          } else {
+            return item;
+          }
+        });
+
+        // Mettre à jour le document avec les nouvelles valeurs
+        docRef.update({
+          values: updatedValues
+        })
+        .then(() => {
+          console.log("Checked state updated for value: ", value);
+        })
+        .catch((error) => {
+          console.error("Error updating document: ", error);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting document: ", error);
+    });
+
 }
